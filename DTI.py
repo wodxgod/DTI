21bdb3a62a1d9a1a27968582637ccfe90ac5925c
wodx <38795504+WodxTV@users.noreply.github.com>
2019-12-24 02:06:27 +0100

    Update DTI.py

--- DTI.py
+++ DTI.py
@@ -1,42 +1,47 @@
 import requests
 import json
-import os
-import subprocess
 import sys
+import datetime
 
 from colorama import Fore, init
 
-__version__ = 1.7
+__version__ = 1.8
 
 languages = {
-    'da'    : 'Danish',
-    'de'    : 'German',
-    'en-GB' : 'English, UK',
-    'en-US' : 'English, US',
-    'es-ES' : 'Spanish',
-    'fr'    : 'French',
-    'hr'    : 'Croatian',
-    'lt'    : 'Lithuanian',
-    'hu'    : 'Hungarian',
-    'nl'    : 'Dutch',
-    'no'    : 'Norwegian',
-    'pl'    : 'Polish',
-    'pt-BR' : 'Portuguese, Brazilian',
+    'da'    : 'Danish, Denmark',
+    'de'    : 'German, Germany',
+    'en-GB' : 'English, United Kingdom',
+    'en-US' : 'English, United States',
+    'es-ES' : 'Spanish, Spain',
+    'fr'    : 'French, France',
+    'hr'    : 'Croatian, Croatia',
+    'lt'    : 'Lithuanian, Lithuania',
+    'hu'    : 'Hungarian, Hungary',
+    'nl'    : 'Dutch, Netherlands',
+    'no'    : 'Norwegian, Norway',
+    'pl'    : 'Polish, Poland',
+    'pt-BR' : 'Portuguese, Brazilian, Brazil',
     'ro'    : 'Romanian, Romania',
-    'fi'    : 'Finnish',
-    'sv-SE' : 'Swedish',
-    'vi'    : 'Vietnamese',
-    'tr'    : 'Turkish',
-    'cs'    : 'Czech',
-    'el'    : 'Greek',
-    'bg'    : 'Bulgarian',
-    'ru'    : 'Russian',
-    'uk'    : 'Ukranian',
-    'th'    : 'Thai',
+    'fi'    : 'Finnish, Finland',
+    'sv-SE' : 'Swedish, Sweden',
+    'vi'    : 'Vietnamese, Vietnam',
+    'tr'    : 'Turkish, Turkey',
+    'cs'    : 'Czech, Czechia, Czech Republic',
+    'el'    : 'Greek, Greece',
+    'bg'    : 'Bulgarian, Bulgaria',
+    'ru'    : 'Russian, Russia',
+    'uk'    : 'Ukranian, Ukraine',
+    'th'    : 'Thai, Thailand',
     'zh-CN' : 'Chinese, China',
     'ja'    : 'Japanese',
     'zh-TW' : 'Chinese, Taiwan',
-    'ko'    : 'Korean'
+    'ko'    : 'Korean, Korea'
+}
+
+cc_digits = {
+    'american express': '3',
+    'visa': '4',
+    'mastercard': '5'
 }
 
 def main():
@@ -58,24 +63,20 @@ def main():
         token = sys.argv[1]
 
         try:
-            # sends request
-            res = requests.get(
-                url='https://discordapp.com/api/v6/users/@me',
-                headers={
-                    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/53',
-                    'Authorization': token,
-                    'Content-Type': 'application/json'
-                }
-            )
-                
-            if res.status_code == 200: # code 200 if valid
-                res_json = json.loads(res.text)
+            headers = {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
 
+            res = requests.get('https://discordapp.com/api/v6/users/@me', headers=headers)
+            if res.status_code == 200: # code 200 if valid
+
+                # user info
+                res_json = res.json()
                 user_name = f'{res_json["username"]}#{res_json["discriminator"]}'
                 user_id = res_json['id']
                 avatar_id = res_json['avatar']
-                avatar_url = f'https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}.webp'
-                mention = f'<@{user_id}>'
+                avatar_url = f'https://cdn.discordapp.com/avatars/{user_id}/{avatar_id}'
                 phone_number = res_json['phone']
                 email = res_json['email']
                 mfa_enabled = res_json['mfa_enabled']
@@ -83,14 +84,66 @@ def main():
                 locale = res_json['locale']
                 verified = res_json['verified']
                 language = languages.get(locale)
+                creation_date = datetime.datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime('%d-%m-%Y %H:%M:%S UTC')
+                
+                # billing info
+                billing_info = []
+                for x in requests.get('https://discordapp.com/api/v6/users/@me/billing/payment-sources', headers=headers).json():
+                    y = x['billing_address']
+                    name = y['name']
+                    address_1 = y['line_1']
+                    address_2 = y['line_2']
+                    city = y['city']
+                    postal_code = y['postal_code']
+                    state = y['state']
+                    country = y['country']
+
+                    if x['type'] == 1:
+                        cc_brand = x['brand']
+                        cc_first = cc_digits.get(cc_brand)
+                        cc_last = x['last_4']
+                        cc_month = str(x['expires_month'])
+                        cc_year = str(x['expires_year'])
+                        
+                        data = {
+                            'Payment Type': 'Credit Card',
+                            'Valid': not x['invalid'],
+                            'CC Holder Name': name,
+                            'CC Brand': cc_brand.title(),
+                            'CC Number': ''.join(z if (i + 1) % 2 else z + ' ' for i, z in enumerate((cc_first if cc_first else '*') + ('*' * 11) + cc_last)),
+                            'CC Exp. Date': ('0' + cc_month if len(cc_month) < 2 else cc_month) + '/' + cc_year[2:4],
+                            'Address 1': address_1,
+                            'Address 2': address_2 if address_2 else '',
+                            'City': city,
+                            'Postal Code': postal_code,
+                            'State': state if state else '',
+                            'Country': country,
+                            'Default Payment Method': x['default']
+                        }
+
+                    elif x['type'] == 2:
+                        data = {
+                            'Payment Type': 'PayPal',
+                            'Valid': not x['invalid'],
+                            'PayPal Name': name,
+                            'PayPal Email': x['email'],
+                            'Address 1': address_1,
+                            'Address 2': address_2 if address_2 else '',
+                            'City': city,
+                            'Postal Code': postal_code,
+                            'State': state if state else '',
+                            'Country': country,
+                            'Default Payment Method': x['default']
+                        }
+
+                    billing_info.append(data)
 
                 print('Basic Information')
                 print('-----------------')
                 print(f'    {Fore.RESET}User Name              {Fore.GREEN}{user_name}')
                 print(f'    {Fore.RESET}User ID                {Fore.GREEN}{user_id}')
-                print(f'    {Fore.RESET}Avatar ID              {Fore.GREEN}{avatar_id if avatar_id else ""}')
                 print(f'    {Fore.RESET}Avatar URL             {Fore.GREEN}{avatar_url if avatar_id else ""}')
-                print(f'    {Fore.RESET}Mention                {Fore.GREEN}{mention}')
+                print(f'    {Fore.RESET}Date of Creation       {Fore.GREEN}{creation_date}')
                 print(f'    {Fore.RESET}Token                  {Fore.GREEN}{token}')
                 print(f'{Fore.RESET}\n')
 
@@ -99,6 +152,30 @@ def main():
                 print(f'    {Fore.RESET}Phone Number           {Fore.YELLOW}{phone_number if phone_number else ""}')
                 print(f'    {Fore.RESET}Email                  {Fore.YELLOW}{email if email else ""}')
                 print(f'{Fore.RESET}\n')
+
+                if len(billing_info) > 0:
+                    print('Billing Information')
+                    print('-------------------')
+                    if len(billing_info) == 1:
+                        for x in billing_info:
+                            for key, val in x.items():
+                                if not val:
+                                    continue
+                                print(Fore.RESET + '    {:<23}{}{}'.format(key, Fore.CYAN, val))
+                        print(f'{Fore.RESET}\n')
+                    else:
+                        for i, x in enumerate(billing_info):
+                            payment_method = x['Payment Type']
+                            title = f'Payment Method #{i + 1} ({payment_method})'
+                            print('    ' + title)
+                            print('    ' + ('=' * len(title)))
+                            for j, (key, val) in enumerate(x.items()):
+                                if not val or j == 0:
+                                    continue
+                                print(Fore.RESET + '        {:<23}{}{}'.format(key, Fore.CYAN, val))
+                            if i < len(billing_info) - 1:
+                                print(f'{Fore.RESET}\n')
+                        print(f'{Fore.RESET}\n')
 
                 print('Account Security')
                 print('----------------')
@@ -109,7 +186,7 @@ def main():
                 print('Other')
                 print('-----')
                 print(f'    {Fore.RESET}Locale                 {Fore.RED}{locale} ({language})')
-                print(f'    {Fore.RESET}Verified               {Fore.RED}{verified}')
+                print(f'    {Fore.RESET}Email Verified         {Fore.RED}{verified}')
 
             elif res.status_code == 401: # code 401 if invalid
                 print(f'{Fore.RED}[-] {Fore.RESET}Invalid token')
@@ -119,7 +196,7 @@ def main():
         except:
             print(f'{Fore.RED}[-] {Fore.RESET}An error occurred while sending request')
     else:
-        print(f'Usage: python {sys.argv[0]} <token>')
+        print(f'Usage: python {sys.argv[0]} [token]')
 
 if __name__ == '__main__':
     main()
